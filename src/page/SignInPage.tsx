dfd018822968c5672942e0918c6bbaa5aa282a50
raoul2000 <manu34@free.fr>
2021-07-28 00:12:36 +0200

    ui:improve signin form

--- src/page/SignInPage.tsx
+++ src/page/SignInPage.tsx
@@ -1,22 +1,19 @@
 import React, { useState } from "react";
-import Avatar from "@material-ui/core/Avatar";
 import Button from "@material-ui/core/Button";
 import CssBaseline from "@material-ui/core/CssBaseline";
 import TextField from "@material-ui/core/TextField";
-import FormControlLabel from "@material-ui/core/FormControlLabel";
-import Checkbox from "@material-ui/core/Checkbox";
-import Link from "@material-ui/core/Link";
-import Grid from "@material-ui/core/Grid";
-import LockOutlinedIcon from "@material-ui/icons/LockOutlined";
 import Typography from "@material-ui/core/Typography";
 import { makeStyles } from "@material-ui/core/styles";
 import Container from "@material-ui/core/Container";
+import Snackbar from "@material-ui/core/Snackbar";
+import IconButton from "@material-ui/core/IconButton";
+import CloseIcon from "@material-ui/icons/Close";
 
 import BookApi from "../api/book";
-import {useLocation} from 'wouter';
+import { useLocation } from "wouter";
 import { useSetRecoilState } from "recoil";
 import { apiKeyState } from "@/state/api-key";
-import Storage from '@/utils/storage';
+import Storage from "@/utils/storage";
 
 const useStyles = makeStyles((theme) => ({
     paper: {
@@ -38,25 +35,62 @@ const useStyles = makeStyles((theme) => ({
     },
 }));
 
+type FormError = {
+    username: boolean;
+    password: boolean;
+};
+
 export const SignInPage: React.FC<{}> = (): JSX.Element => {
     const classes = useStyles();
+    const [openSnackBar, setOpenSnackBar] = React.useState(false);
     const [, setLocation] = useLocation();
     const setApiKey = useSetRecoilState(apiKeyState);
+    const [formError, setFormError] = useState<FormError>({
+        username: false,
+        password: false,
+    });
 
-    const [username, setUsername] = useState<string>('');
-    const [password, setpassword] = useState<string>('');
-    
-    const handleLogin = () => {
-        BookApi.login(username, password)
-        .then((apiKey) => {
-            setApiKey(apiKey);
-            Storage.setApiKey(apiKey);
-            setLocation('/');
-        })
-        .catch(err => {
-            alert(`erreur : ${err.message}`);
-            console.error(err);
+    const [username, setUsername] = useState<string>("");
+    const [password, setpassword] = useState<string>("");
+
+    const handleCloseSnackBar = (
+        event: React.SyntheticEvent | React.MouseEvent,
+        reason?: string
+    ) => {
+        if (reason === "clickaway") {
+            return;
+        }
+
+        setOpenSnackBar(false);
+    };
+    const isFormValid = () => {
+        if (!username || !password) {
+            setFormError({
+                username: !username,
+                password: !password,
+            });
+            return false;
+        }
+        setFormError({
+            username: false,
+            password: false,
         });
+        return true;
+    };
+
+    const handleLogin = () => {
+        if (isFormValid()) {
+            BookApi.login(username, password)
+                .then((apiKey) => {
+                    setApiKey(apiKey);
+                    Storage.setApiKey(apiKey);
+                    setLocation("/");
+                })
+                .catch((err) => {
+                    setOpenSnackBar(true);
+                    console.error(err);
+                });
+        }
     };
 
     return (
@@ -77,6 +111,7 @@ export const SignInPage: React.FC<{}> = (): JSX.Element => {
                         name="username"
                         autoComplete="username"
                         autoFocus
+                        error={formError.username}
                         onChange={(e) => setUsername(e.target.value)}
                     />
                     <TextField
@@ -89,6 +124,7 @@ export const SignInPage: React.FC<{}> = (): JSX.Element => {
                         type="password"
                         id="password"
                         autoComplete="current-password"
+                        error={formError.password}
                         onChange={(e) => setpassword(e.target.value)}
                     />
                     <Button
@@ -103,6 +139,28 @@ export const SignInPage: React.FC<{}> = (): JSX.Element => {
                     </Button>
                 </form>
             </div>
+            <Snackbar
+                anchorOrigin={{
+                    vertical: "bottom",
+                    horizontal: "center",
+                }}
+                open={openSnackBar}
+                autoHideDuration={6000}
+                onClose={handleCloseSnackBar}
+                message="login failed"
+                action={
+                    <React.Fragment>
+                        <IconButton
+                            size="small"
+                            aria-label="close"
+                            color="inherit"
+                            onClick={handleCloseSnackBar}
+                        >
+                            <CloseIcon fontSize="small" />
+                        </IconButton>
+                    </React.Fragment>
+                }
+            />
         </Container>
     );
 };
