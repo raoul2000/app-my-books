7159ebf747385dffa711fb12306ec5e9fc926a6b
raoul2000 <manu34@free.fr>
2021-08-08 00:09:11 +0200

    ui: use notistack to handle snackbars

--- src/page/SettingsPage.tsx
+++ src/page/SettingsPage.tsx
@@ -1,15 +1,12 @@
-import React, { useState } from "react";
+import React  from "react";
 import useLocation from "wouter/use-location";
 import Typography from "@material-ui/core/Typography";
-import Snackbar from "@material-ui/core/Snackbar";
 import { FormSettings } from "../component/FormSettings";
 import Container from "@material-ui/core/Container";
 
 export const SettingsPage: React.FC<{}> = (): JSX.Element => {
     const [, setLocation] = useLocation();
-    const [showNotif, setShowNotif] = useState(false);
     const goHome = () => {
-        setShowNotif(true);
         setLocation("/");
     };
 
@@ -21,17 +18,7 @@ export const SettingsPage: React.FC<{}> = (): JSX.Element => {
                         <Typography variant="h5" component="h1">
                             Settings
                         </Typography>
-
                         <FormSettings onSubmit={() => goHome()} />
-                        <Snackbar
-                            anchorOrigin={{
-                                vertical: "bottom",
-                                horizontal: "left",
-                            }}
-                            open={false}
-                            autoHideDuration={6000}
-                            message="Note archived"
-                        />
                     </div>
                 </Container>
             </main>
