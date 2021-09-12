4b5ae05154081e7f7cb0f964f1046d5fb6966845
raoul2000 <manu34@free.fr>
2021-09-12 23:24:41 +0200

    use hash location with wouter (fail)

--- src/App.tsx
+++ src/App.tsx
@@ -3,12 +3,13 @@ import "./App.less";
 import "@fontsource/roboto";
 
 import { useRecoilState } from "recoil";
-import { useLocation } from "wouter";
+import { useLocation, Router } from "wouter";
 
 import { WithNavigation } from "./component/WithNavigation";
 import Storage from "@/utils/storage";
 import { apiKeyState } from "./state/api-key";
 import { SnackbarProvider } from "notistack";
+import {useHashLocation} from '@/hooks/useHashLocation';
 
 function App() {
     
@@ -24,11 +25,13 @@ function App() {
     }, [apiKey]);
 
     return (
+        <Router hook={useHashLocation}>
         <SnackbarProvider>
             <div className="App">
                 <WithNavigation />
             </div>
         </SnackbarProvider>
+        </Router>
     );
 }
 export default App;
