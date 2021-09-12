783bbdfcaa49d5e449a287e57f0a3282fd1a9a6a
raoul2000 <manu34@free.fr>
2021-09-12 23:57:37 +0200

    move router root element to parent element (main)

--- src/App.tsx
+++ src/App.tsx
@@ -3,16 +3,14 @@ import "./App.less";
 import "@fontsource/roboto";
 
 import { useRecoilState } from "recoil";
-import { useLocation, Router } from "wouter";
+import { useLocation } from "wouter";
 
 import { WithNavigation } from "./component/WithNavigation";
 import Storage from "@/utils/storage";
 import { apiKeyState } from "./state/api-key";
 import { SnackbarProvider } from "notistack";
-import {useHashLocation} from '@/hooks/useHashLocation';
 
 function App() {
-    
     const [apiKey, setApiKey] = useRecoilState(apiKeyState);
     const [, setLocation] = useLocation();
 
@@ -25,13 +23,11 @@ function App() {
     }, [apiKey]);
 
     return (
-        <Router hook={useHashLocation}>
         <SnackbarProvider>
             <div className="App">
                 <WithNavigation />
             </div>
         </SnackbarProvider>
-        </Router>
     );
 }
 export default App;
